964fdd7a336c4fb3f99911627425b600b0197428
Jan-Niklas Müller <jan-niklas.mueller@hm.edu>
2025-11-20 18:42:18 +0100

    feat: implement web permission bypass attack

---
+++ web-permissions-demo-site/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    https: false,
+    host: false, 
+    allowedHosts: [
+        // add your ngrok URL here
+      "painstaking-gutsily-tamar.ngrok-free.dev",
+    ]
+  },
+});
