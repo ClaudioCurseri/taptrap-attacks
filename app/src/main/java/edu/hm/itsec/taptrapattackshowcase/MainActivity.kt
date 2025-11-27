746b68a7b6a7bd2f6d7ea952b8a864672b6d3672
Claudio Curseri <claudio.curseri@outlook.de>
2025-11-27 18:15:58 +0100

    refactor: remove all unused components

--- app/src/main/java/edu/hm/itsec/taptrapattackshowcase/MainActivity.kt
+++ app/src/main/java/edu/hm/itsec/taptrapattackshowcase/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import edu.hm.itsec.taptrapattackshowcase.apptouchjacking.AppTouchjackingActivity
 import edu.hm.itsec.taptrapattackshowcase.clickjacking.ClickjackingActivity
-import edu.hm.itsec.taptrapattackshowcase.controluielements.ControlUiElementsActivity
 import edu.hm.itsec.taptrapattackshowcase.databinding.ActivityMainBinding
-import edu.hm.itsec.taptrapattackshowcase.deviceadmin.DeviceAdminActivity
-import edu.hm.itsec.taptrapattackshowcase.displayoverapps.DisplayOverAppsActivity
 import edu.hm.itsec.taptrapattackshowcase.notificationservice.NotificationServiceActivity
 import edu.hm.itsec.taptrapattackshowcase.runtimepermissions.RuntimePermissionsActivity
 import edu.hm.itsec.taptrapattackshowcase.web.WebActivity
@@ -36,31 +32,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpButtons() {
-        binding.btnAppTouchjacking.setOnClickListener {
-            val intent = Intent(applicationContext, AppTouchjackingActivity::class.java)
-            startActivity(intent)
-        }
         binding.btnClickjacking.setOnClickListener {
             val intent = Intent(applicationContext, ClickjackingActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnControlUiElements.setOnClickListener {
-            val intent = Intent(applicationContext, ControlUiElementsActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnDeviceAdmin.setOnClickListener {
-            val intent = Intent(applicationContext, DeviceAdminActivity::class.java)
-            startActivity(intent)
-        }
-        binding.btnDisplayOverApps.setOnClickListener {
-            val intent = Intent(applicationContext, DisplayOverAppsActivity::class.java)
             startActivity(intent)
         }
         binding.btnNotificationService.setOnClickListener {
             val intent = Intent(applicationContext, NotificationServiceActivity::class.java)
             startActivity(intent)
         }
-
         binding.btnRuntimePermissions.setOnClickListener {
             val intent = Intent(applicationContext, RuntimePermissionsActivity::class.java)
             startActivity(intent)
